f784b57b52d7320be02b204d577191f5c510125d
sarahmr63089 <sarahrose63089@gmail.com>
2020-08-05 17:05:32 -0400

    rest of lab

--- src/index.js
+++ src/index.js
@@ -19,3 +19,50 @@ function turnImageIntoHTML(json) {
     dogImagePlace.append(blankImg);
   }
 }
+
+fetch('https://dog.ceo/api/breeds/list/all')
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(json){
+    addBreedToUl(json)
+  })
+
+let breedUl = document.querySelector("#dog-breeds")
+
+function addBreedToUl(json) {
+  let arrBreeds = Object.keys(json.message)
+
+  for (let i = 0; i < arrBreeds.length; i++) {
+    let dogBreed = document.createElement("li"); // KEY!!!
+    dogBreed.innerText = arrBreeds[i];
+    breedUl.append(dogBreed);
+
+    dogBreed.addEventListener("click", (evt) => {
+      dogBreed.style.color = "green"
+    })     
+  }
+}
+
+let breedDropdown = document.querySelector("#breed-dropdown")
+
+breedDropdown.addEventListener("change", (evt) => {
+  // grab all lis
+  let dogLis = document.querySelectorAll("li")
+
+  let selection = evt.target.value
+  
+  let allBreeds = []
+
+  for(let i = 0; i < dogLis.length; i++) {
+    if (dogLis[i].nodeName === "LI")
+      allBreeds.push(dogLis[i].innerText)
+  }
+
+  let filteredBreeds = allBreeds.filter((breed) => {
+    return breed.startsWith(selection)
+  })
+  debugger 
+  breedUl.innerHTML = filteredBreeds.map((breed) => `<li>${breed}</li>`).join('')
+  
+})
